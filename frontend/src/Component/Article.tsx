ce2cf33d40aafea210c4373952368330b655d6a2
Gbogboade Adegbenga <allcrownworks@gmail.com>
2024-10-04 12:58:27 +0100

    delered

--- frontend/src/Component/Article.tsx
+++ frontend/src/Component/Article.tsx
@@ -1,20 +1,7 @@
-// import { createApi, fetchBaseQuery, FetchBaseQuery } from "@reduxjs/toolkit/query";
 
-// const options = {
-// 	method: 'GET',
-// 	hostname: 'article-extractor-and-summarizer.p.rapidapi.com',
-// 	port: null,
-// 	path: '/summarize?url=https%3A%2F%2Ftime.com%2F6266679%2Fmusk-ai-open-letter%2F&lang=en&engine=2',
-// 	headers: {
-// 		'x-rapidapi-key': 'ed50f61f3bmsh8aba25801f09ef7p13f907jsnf356d36a9e96',
-// 		'x-rapidapi-host': 'article-extractor-and-summarizer.p.rapidapi.com'
-// 	}
-// };
-
-// -------------------------new code by gpt -------------------------------
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const rapidApiKey = 'ed50f61f3bmsh8aba25801f09ef7p13f907jsnf356d36a9e96';  // Your API key
+
 const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;  // Access the API key from the .env file
 
 export const articleApi = createApi({
